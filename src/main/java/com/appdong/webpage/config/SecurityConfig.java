0f4e283b82f7af20af9f3976f0a900b85ca7696d
Koo-EunSung <ku903@naver.com>
2024-08-19 10:22:03 +0900

    Feat: Security Config 추가

---
+++ src/main/java/com/appdong/webpage/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.appdong.webpage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest.
+                                anyRequest().
+                                permitAll()
+                );
+
+        return http.build();
+    }
+}
